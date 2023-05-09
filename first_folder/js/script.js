4fb59f771ee0761a2b766aacbb065d1d31e16568
Evgeny <dartveider87@gmail.com>
2023-05-09 16:31:34 +0300

    first commit

---
+++ first_folder/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(--incr, ++decr);
+
+console.log((2 + 2) * 2 == 8)
+
+/* const isChecked = false,
+      isClosed = false;
+
+console.log(isChecked || !isClosed) */
