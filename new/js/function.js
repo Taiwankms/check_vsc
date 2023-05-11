1e2fe38a00d87f9306de35094be8f7dce5a55f8d
Evgeny <dartveider87@gmail.com>
2023-05-11 17:48:43 +0300

    learn set

--- new/js/function.js
+++ new/js/function.js
@@ -48,4 +48,28 @@ let numb = '4.5px';
 //console.log(Math.round(numb))
 console.log(parseFloat(numb))
 
+function first() {
+    // Do something
+    setTimeout(function() {
+        console.log(1)
+    }, 500);
+};
 
+function second() {
+    console.log(2)
+};
+
+first();
+second();
+
+
+function learnJs(lang, callback) {
+    console.log(`Я учу ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('я прошел этот урок');
+}
+
+learnJs('JavaScript', done)
--- new/js/script.js
+++ new/js/script.js
@@ -21,14 +21,14 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
     let count = 1;
-do {   
-    const a = prompt('Один из последних фильмов которые видели', ''),
-          b = prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[a] = b;
-    if (a.length != 0 && b.length != 0 && a.length < 50 && a != null && b != null) {
-        count++
-    }         
-} while (count < 3);
+    do {
+        const a = prompt('Один из последних фильмов которые видели', ''),
+            b = prompt('На сколько оцените его?', '');
+        personalMovieDB.movies[a] = b;
+        if (a.length != 0 && b.length != 0 && a.length < 50 && a != null && b != null) {
+            count++
+        }
+    } while (count < 3);
 
 }
 
@@ -44,25 +44,78 @@ function detectPersonalLevel() {
     } else {
         alert("вы ввели херню!");
     };
-    
+
 }
 
 //detectPersonalLevel();
 
-function showMyDB (hidden) {
+function showMyDB(hidden) {
     if (!hidden) {
         console.log(personalMovieDB)
     }
-    
+
 }
 
 showMyDB(personalMovieDB.privat)
 
 function writeYourFavoriteGaners() {
-    for (let i = 1; i <= 3; i++) {        
-        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
     }
 }
 
 writeYourFavoriteGaners();
 
+let options = {
+    name: 'test',
+    width: 1024,
+    heigth: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red',
+    },
+    makeTest: function () {
+        console.log('Hello test')
+    }
+
+};
+let descriptor = Object.getOwnPropertyDescriptor(options, 'heigth');
+Object.defineProperties(options, 'heigth', descriptor)
+console.log(JSON.stringify(descriptor, null, 2));
+const { border, bg } = options.colors
+console.log(border, bg)
+
+options.makeTest()
+
+console.log(Object.keys(options).length)
+//console.log(options['colors']['border']);
+//
+//delete obj.name;
+//
+//console.log(obj);
+
+
+for (let key in options) {
+    if (typeof (options[key]) === 'object') {
+        for (i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`)
+    }
+}
+console.log(count)
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8]
+arr[99] = 0;
+arr
+//arr.pop()
+//arr.push('nine')
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
+for (let val of arr) {
+    console.log(val)
+}
+
+console.log(arr)
